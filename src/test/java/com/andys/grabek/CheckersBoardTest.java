91c316a92bea2ec3de1fd7deef09532d61338a5e
Andrzej Grabowski <andys.grabek@icloud.com>
2019-03-08 12:47:25 +0100

    Updated CheckersBoard test

--- src/test/java/com/andys/grabek/CheckersBoardTest.java
+++ src/test/java/com/andys/grabek/CheckersBoardTest.java
@@ -5,10 +5,25 @@ import main.java.com.andys.grabek.CheckersPlayer;
 import main.java.com.andys.grabek.CheckersPlayerOne;
 import main.java.com.andys.grabek.CheckersPlayerTwo;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CheckersBoardTest {
+
+    private static final String board = "" +
+            "  0 1 2 3 4 5 6 7  \n" +
+            " +----------------+\n" +
+            "0|  1   1   1   1 |\n" +
+            "1|1   1   1   1   |\n" +
+            "2|  1   1   1   1 |\n" +
+            "3|                |\n" +
+            "4|                |\n" +
+            "5|2   2   2   2   |\n" +
+            "6|  2   2   2   2 |\n" +
+            "7|2   2   2   2   |\n" +
+            " +----------------+\n" +
+            "  0 1 2 3 4 5 6 7  \n";
 
     private CheckersPlayer playerOne;
     private CheckersPlayer playerTwo;
@@ -21,38 +36,44 @@ class CheckersBoardTest {
         checkersBoard = new CheckersBoard(playerOne, playerTwo);
     }
 
-    @org.junit.jupiter.api.Test
-    void numberOfPieces() {
+    @Test
+    void numberOfPiecesTest() {
         checkersBoard.clearField(1, 2);
         assertEquals(11, checkersBoard.numberOfPieces(playerOne));
     }
 
-    @org.junit.jupiter.api.Test
-    void getPlayerOnField() {
+    @Test
+    void getPlayerOnFieldTest() {
         assertEquals(0, checkersBoard.getPlayerOnField(2, 2));
         assertEquals(playerOne.getNumericalValue(), checkersBoard.getPlayerOnField(3, 2));
         assertEquals(playerTwo.getNumericalValue(), checkersBoard.getPlayerOnField(6, 7));
     }
 
-    @org.junit.jupiter.api.Test
-    void putPlayerOnField() {
+    @Test
+    void putPlayerOnFieldTest() {
         checkersBoard.putPlayerOnField(0, 0 ,playerOne);
         assertEquals(playerOne.getNumericalValue(), checkersBoard.getPlayerOnField(0, 0));
     }
 
-    @org.junit.jupiter.api.Test
-    void clearField() {
+    @Test
+    void clearFieldTest() {
         checkersBoard.clearField(0, 0);
         assertEquals(0, checkersBoard.getPlayerOnField(0, 0));
     }
 
-    @org.junit.jupiter.api.Test
-    void performMove() {
+    @Test
+    void performMoveTest() {
         assertTrue(checkersBoard.performMove(3, 2, 4, 3, playerOne));
         assertFalse(checkersBoard.performMove(3, 2, 4, 3, playerOne));
         assertTrue(checkersBoard.performMove(0, 5, 1, 4, playerTwo));
         assertFalse(checkersBoard.performMove(0, 5, 1, 4, playerTwo));
         assertTrue(checkersBoard.performMove(1, 2, 2,3, playerOne));
         assertTrue(checkersBoard.performMove(1, 4, 3,2, playerTwo));
+        assertFalse(checkersBoard.performMove(-1, 0, 17, 2, playerTwo));
+    }
+
+    @Test
+    void toStringTest() {
+        assertEquals(board, checkersBoard.toString());
     }
 }
