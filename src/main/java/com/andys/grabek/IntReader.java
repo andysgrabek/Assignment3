56ff3fff0af0b5cdaf77b5c4a620270e874ca24c
Andrzej Grabowski <andys.grabek@icloud.com>
2019-03-08 14:30:00 +0100

    Refactored IntReader and CheckersBoardPrinter

--- src/main/java/com/andys/grabek/IntReader.java
+++ src/main/java/com/andys/grabek/IntReader.java
@@ -8,18 +8,24 @@ import java.util.Scanner;
  * Class used to read verified integer input from the given input stream
  */
 public class IntReader {
+
+    private InputStream is;
+
+    public IntReader(InputStream is) {
+        this.is = is;
+    }
+
     /**
      * Prompts the user to enter an integer number via given input stream until a valid int is provided
      *
-     * @param in selected input stream
      * @param message text to be displayed when prompting the user to enter the number
      * @return user-provided int value
      */
-    public static int getIntInput(InputStream in, String message) {
+    public int getIntInput(String message) {
         while(true) {
             try {
                 System.out.println("\t" + message);
-                Scanner sc = new Scanner(in);
+                Scanner sc = new Scanner(is);
                 return sc.nextInt();
             } catch (InputMismatchException e) {
                 System.out.println("Input must be a non-negative integer number");
